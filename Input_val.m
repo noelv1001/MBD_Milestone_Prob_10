c4786268f403b5c171cfa5ac64329c0733fb962d
Noel Varghese <Noel Varghese@Noel>
2020-12-07 12:20:42 +0530

    Tried running with contant voltage value, got corresponding current value.

--- Input_val.m
+++ Input_val.m
@@ -1,7 +1,12 @@
-%Program to input voltage and current values of Diode
+%Program to find voltage and current values of Diode
 clearvars;
 I0=2.5e-12;
 Vt=0.02586;
 Vd= -1:0.01:1;
 I=I0*(exp(Vd/(2*Vt)));
 plot(Vd,I);
+
+%Program to input voltage values
+t=(0:0.01:10)';
+volt=sin(t);
+sim('MBD_Prob10.slx');
